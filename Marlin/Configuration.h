04abf1267547f1d35b06a97877102e346e154bad
Kevin Wong <25396158+oishii-code@users.noreply.github.com>
2022-07-17 20:20:21 -0700

    Update hotend PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -655,9 +655,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  26.45
+    #define DEFAULT_Ki   3.26
+    #define DEFAULT_Kd  53.65
   #endif
 #endif
 
