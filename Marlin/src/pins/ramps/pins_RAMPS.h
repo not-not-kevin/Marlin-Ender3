848f037a123ec65a375041cce88174fe59b82ba8
Kevin Wong <25396158+oishii-code@users.noreply.github.com>
2023-01-21 17:26:25 -0800

    Setup chamber thermistor

--- Marlin/src/pins/ramps/pins_RAMPS.h
+++ Marlin/src/pins/ramps/pins_RAMPS.h
@@ -201,7 +201,8 @@
   #define TEMP_0_PIN                          13  // Analog Input
 #endif
 #ifndef TEMP_1_PIN
-  #define TEMP_1_PIN                          15  // Analog Input
+  //#define TEMP_1_PIN                          15  // Analog Input
+  #define TEMP_CHAMBER_PIN 15  // Analog Input
 #endif
 #ifndef TEMP_BED_PIN
   #define TEMP_BED_PIN                        14  // Analog Input
